fb081fcd6615ecdcad20311b986f985416268291
Marc Delorme <m-delorme@bns-g.com>
2019-01-08 17:03:43 +0900

    Test different container instanciate different singleton

--- source/boost_di/main.cpp
+++ source/boost_di/main.cpp
@@ -2,16 +2,24 @@
 
 #include <test/gen_test.h>
 
+#include <assert.h>
+
 int main() {
     auto injector = boost::di::make_injector(boost::di::bind<MyClass_0_0>().in(boost::di::singleton));
 
-    MyClass_0_0& instanceA = injector.create<MyClass_0_0&>();
-    MyClass_0_0& instanceB = injector.create<MyClass_0_0&>();
+    MyClass_0_0* instance1 = &injector.create<MyClass_0_0&>();
+    MyClass_0_0* instance2 = &injector.create<MyClass_0_0&>();
+
+    assert(instance1 == instance2);
     
     auto injector2 = boost::di::make_injector(boost::di::bind<MyClass_0_0>().in(boost::di::singleton));
 
-    MyClass_0_0& instanceA2 = injector2.create<MyClass_0_0&>();
-    MyClass_0_0& instanceB2 = injector2.create<MyClass_0_0&>();
+    MyClass_0_0* instance3 = &injector2.create<MyClass_0_0&>();
+    MyClass_0_0* instance4 = &injector2.create<MyClass_0_0&>();
+
+    assert(instance3 == instance4);
+
+    assert(instance1 != instance4);
     
     return 0;
 }
--- source/kangaru/main.cpp
+++ source/kangaru/main.cpp
@@ -1,10 +1,23 @@
 #include <test/gen_test.kangaru.h>
 
-#include <iostream>
+#include <assert.h>
 
 int main()
 {
     kgr::container container;
 
-    MyClass_0_0& instance = container.service<MyClass_0_0Service>();
+    MyClass_0_0* instance1 = &container.service<MyClass_0_0Service>();
+    MyClass_0_0* instance2 = &container.service<MyClass_0_0Service>();
+
+    assert(instance1 == instance2);
+
+    kgr::container container2;
+    MyClass_0_0* instance3 = &container2.service<MyClass_0_0Service>();
+    MyClass_0_0* instance4 = &container2.service<MyClass_0_0Service>();
+
+    assert(instance3 == instance4);
+
+    assert(instance1 != instance4);
+
+    return 0;
 }
